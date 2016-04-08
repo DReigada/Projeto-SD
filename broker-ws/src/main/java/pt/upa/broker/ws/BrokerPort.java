b7d3a4e6ee9f7649e243889420efb97993070778
DiMesq <d.mesquita285@gmail.com>
2016-04-08 12:39:30 +0100

    Creates (doesn't implement) the brokerPort class that implements the service interface defined in the wsdl.

--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -1,7 +1,44 @@
 package pt.upa.broker.ws;
 
-public class BrokerPort {
+import javax.jws.WebService;
 
-	// TODO
+@WebService(
+  endpointInterface="pt.upa.broker.ws.BrokerPortType",
+  wsdlLocation="broker.1_0.wsdl",
+  name="BrokerWebService",
+  portName="BrokerPort",
+  targetNamespace="http://ws.broker.upa.pt/",
+  serviceName="BrokerService"
+)
+public class BrokerPort implements BrokerPortType {
 
+  @Override
+  public String ping(String name){
+    return "Alive: " + name;
+  }
+
+  @Override
+  public String requestTransport(String origin, String destination, int price) throws 
+    UnknownLocationFault_Exception,
+    InvalidPriceFault_Exception,
+    UnavailableTransportFault_Exception,
+    UnavailableTransportPriceFault_Exception {
+
+  }
+
+  @Override
+  public TransportView viewTransport(String id) throws 
+    UnknownLocationFault_Exception {
+
+  }
+
+  @Override
+  public Iterable listTransports() {
+
+  }
+
+  @Override
+  public void clearTransports() {
+
+  }
 }
