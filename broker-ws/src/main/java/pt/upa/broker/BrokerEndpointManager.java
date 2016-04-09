72e07466794c14396870dc1d466a21e37e33c775
DiMesq <d.mesquita285@gmail.com>
2016-04-09 12:33:22 +0100

    Implements broker server main that starts the server and registers in juddi.

---
+++ broker-ws/src/main/java/pt/upa/broker/BrokerEndpointManager.java
@@ -0,0 +1,69 @@
+package pt.upa.broker;
+
+import pt.upa.broker.ws.BrokerPort;
+
+import javax.xml.ws.Endpoint;
+
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+
+import javax.xml.registry.JAXRException;
+
+public class BrokerEndpointManager {
+
+  private String _uddiURL;
+  private String _name;
+  private String _url;
+  private Endpoint _endpoint;
+  private UDDINaming _uddiNaming;
+  
+  public BrokerEndpointManager(String uddiURL) {
+    _uddiURL = uddiURL;
+    _endpoint = null;
+    _name = _url = null;
+    _uddiNaming = null;
+  }
+
+  public void start(String url) {
+    _url = url;
+    
+    BrokerPort port = new BrokerPort();
+    _endpoint = Endpoint.create(port);
+
+    // publish endpoint
+    System.out.printf("Starting %s%n", _url);
+    _endpoint.publish(_url);
+  }
+
+  public void awaitConnections(String name) throws 
+     JAXRException {
+    // publish to UDDI
+    _uddiNaming = new UDDINaming(_uddiURL);
+    _uddiNaming.rebind(name, _url);
+  }
+
+  public void stop() {
+    try {
+      if (_endpoint != null) {
+        // stop endpoint
+        _endpoint.stop();
+        System.out.printf("Stopped %s%n", _url);
+      }
+    } catch (Exception e) {
+      System.out.printf("Caught exception when stopping: %s%n", e);
+    }
+    try {
+      if (_uddiNaming != null) {
+        // delete from UDDI
+        _uddiNaming.unbind(_name);
+        System.out.printf("Deleted '%s' from UDDI%n", _name);
+      }
+    } catch (Exception e) {
+      System.out.printf("Caught exception when deleting: %s%n", e);
+    }
+
+    _endpoint = null;
+    _name = _url = null;
+    _uddiNaming = null;
+
+  }
+}
