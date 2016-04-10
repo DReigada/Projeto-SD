345d1aa4eee76ad9619ff6720bdde3b60fbb4053
DReigada <daniel.reigada@gmail.com>
2016-04-10 11:22:53 +0100

    Change the Transporter application to use the transporter and enpoint class

--- transporter-ws/src/main/java/pt/upa/transporter/TransporterApplication.java
+++ transporter-ws/src/main/java/pt/upa/transporter/TransporterApplication.java
@@ -1,62 +1,40 @@
 package pt.upa.transporter;
 
-import javax.xml.ws.Endpoint;
-
-import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
-import pt.upa.transporter.ws.TransporterPort;
+import pt.upa.transporter.core.Transporter;
 
 public class TransporterApplication {
 
 	public static void main(String[] args) throws Exception {
+		if (args.length != 3) {
+			System.err.println("Argument(s) missing!");
+			System.err.printf("Usage: java %s uddiURL transporterNumber endpointURL%n", TransporterApplication.class.getName());
+			return;
+		}
+		
 		System.out.println(TransporterApplication.class.getSimpleName() + " starting...");
 
 		
 		String uddiURL = args[0];
-		String name = args[1];
-		String url = args[2];	
-
-		Endpoint endpoint = null;
-		UDDINaming uddiNaming = null;
-
-		try{
-			TransporterPort port = new TransporterPort();
-			endpoint = Endpoint.publish(url, port);
-
-			uddiNaming = new UDDINaming(uddiURL);
-			uddiNaming.rebind(name, url);
-
-
-			System.out.println("Awaiting connections");
-			System.out.println("Press enter to shutdown");
-			System.in.read();
-		}
-		catch (Exception e) {
-			System.out.printf("Caught exception: %s%n", e);
-			e.printStackTrace();
-		}
-		finally {
-				try {
-					if (endpoint != null) {
-						// stop endpoint
-						endpoint.stop();
-						System.out.printf("Stopped %s%n", url);
-					}
-				} catch (Exception e) {
-					System.out.printf("Caught exception when stopping: %s%n", e);
-				}
-				try {
-					if (uddiNaming != null) {
-						// delete from UDDI
-						uddiNaming.unbind(name);
-						System.out.printf("Deleted '%s' from UDDI%n", name);
-					}
-				} catch (Exception e) {
-					System.out.printf("Caught exception when deleting: %s%n", e);
-				}
-		}
-
-
-
+		String ID = args[1];
+		String endpointURL = args[2];	
+		
+		Transporter transporter = new Transporter(Integer.parseInt(ID));
+		TransporterEndpoint endpoint = new TransporterEndpoint(transporter, endpointURL, uddiURL);
+		
+		System.out.printf("Publishing %s endpoint at %s%n", transporter.getName(), endpointURL);
+		endpoint.startEndpoint();
+		System.out.printf("Binding endpoint at %s%n", uddiURL);
+		endpoint.bind();
+		
+		System.out.printf("Awaiting requests%nPress Enter to shutdown...");
+		System.in.read();
+		
+		System.out.printf("Unbinding endpoint from %s%n", uddiURL);
+		endpoint.unbind();
+		System.out.printf("Stoping endpoint%n");
+		endpoint.stopEndpoint();
+		
+		System.out.printf("%s ended successfully%n", transporter.getName());
 	}
 
 }
