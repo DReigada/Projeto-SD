c6d40e07ccac2b48735a3b327d676df6bad0ae4b
joaoplancha <joaoplancha@icloud.com>
2016-04-10 16:01:27 +0100

    broker-cli

--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -1,7 +1,19 @@
 package pt.upa.broker.ws;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
 public class BrokerPort {
+	
+	@WebMethod
+	public String ping(String pingOut) {
+		return "Alive 'n kicking";
+	}
+	
 
-	// TODO
-
+	@WebMethod
+	public void clearTransports(){
+		
+	}
 }
