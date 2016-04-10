8a1c801e92e711b267dc14024202d0ece4178e04
DReigada <daniel.reigada@gmail.com>
2016-04-10 18:13:56 +0100

    Add the Exceptions that the Transporter's methods throw

---
+++ transporter-ws/src/main/java/pt/upa/transporter/core/Exceptions/BadPriceException.java
@@ -0,0 +1,9 @@
+package pt.upa.transporter.core.Exceptions;
+
+@SuppressWarnings("serial")
+public class BadPriceException extends Exception {
+	
+	public BadPriceException(String message) {
+		super(message);
+	}
+}
---
+++ transporter-ws/src/main/java/pt/upa/transporter/core/Exceptions/InvalidCityException.java
@@ -0,0 +1,9 @@
+package pt.upa.transporter.core.Exceptions;
+
+@SuppressWarnings("serial")
+public class InvalidCityException extends Exception {
+	
+	public InvalidCityException(String message) {
+		super(message);
+	}
+}
