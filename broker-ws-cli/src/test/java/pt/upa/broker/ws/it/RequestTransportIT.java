a95b2f3d27bc397c247db6a69d7fd418f2fc091b
DiMesq <d.mesquita285@gmail.com>
2016-04-15 15:09:04 +0100

    Removes the example test

--- broker-ws-cli/src/test/java/pt/upa/broker/ws/it/RequestTransportIT.java
+++ broker-ws-cli/src/test/java/pt/upa/broker/ws/it/RequestTransportIT.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class RequestTransportIT extends BaseBrokerIT{
+public class RequestTransportIT extends BaseBrokerIT {
 	// tests
 	// assertEquals(expected, actual);
 
