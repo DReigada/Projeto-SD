1059a5b41b36e05fc40470a712e4ed361c5187eb
DiMesq <d.mesquita285@gmail.com>
2016-04-15 22:43:07 +0100

    Moves some already existing tests to a new class.

--- broker-ws/src/test/java/pt/upa/broker/ws/ViewTransportTest.java
+++ broker-ws/src/test/java/pt/upa/broker/ws/ViewTransportTest.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 
 import mockit.Expectations;
 import mockit.Verifications;
-import pt.upa.transporter.ws.JobView;
 
 public class ViewTransportTest extends BaseTest {
 	
@@ -190,40 +189,6 @@ public class ViewTransportTest extends BaseTest {
     	}};
     }
 	
-	// TEST: test with two transporters (MOVE)
-	@Test
-    public void testViewTransportWithTwoTransporters() throws Exception{
-    	
-    	new Expectations(){{
-    		manager.getAllTransporterPorts(); result = Arrays.asList(transporter, transporter2);
-    		transporter.requestJob(anyString, anyString, anyInt);
-    		result = _jobView1;
-    		transporter2.requestJob(anyString, anyString, anyInt);
-    		result = _jobViewTransporter2;
-    		
-    		manager.getTransporterPort(anyString); result = transporter;
-    		transporter.jobStatus(anyString); result = _jobViewAccepted;
-    	}};
-    	
-    	_broker.requestTransport(ORIGIN_1, DESTINATION_1, PRICE_3);
-    	TransportView transport1 = _broker.viewTransport(ID_1);
-		
-    	assertEquals(ID_1, transport1.getId());
-		assertEquals(ORIGIN_1, transport1.getOrigin());
-		assertEquals(DESTINATION_1, transport1.getDestination());
-		assertTrue(PRICE_1 == transport1.getPrice());
-		assertEquals(COMPANY_1_NAME, transport1.getTransporterCompany());
-    	
-		
-    	new Verifications() {{
-    		manager.getAllTransporterPorts(); times = 1;
-    		transporter.requestJob(anyString, anyString, anyInt); times = 1;
-    		transporter2.requestJob(anyString, anyString, anyInt); times = 1;
-    		manager.getTransporterPort(anyString); times = 1;
-    		transporter.jobStatus(anyString); times = 1;
-    	}};
-    }
-	
 	// TEST: two valid transports, check one
 	@Test
     public void testViewTransportWithTwoValidTransports() throws Exception{
@@ -320,36 +285,4 @@ public class ViewTransportTest extends BaseTest {
     	}};
     }
 	
-	// TEST: best proposed price = reference price
-	@Test
-    public void testViewTransportEdgeCasePrice() throws Exception{
-    	
-    	new Expectations(){{
-    		manager.getAllTransporterPorts(); result = Arrays.asList(transporter, transporter2);
-    		transporter.requestJob(anyString, anyString, anyInt);
-    		result = _jobView1;
-    		transporter2.requestJob(anyString, anyString, anyInt);
-    		result = _jobViewTransporter2;
-    		
-    		manager.getTransporterPort(anyString); result = transporter;
-    		transporter.jobStatus(anyString); result = _jobViewAccepted;
-    	}};
-    	
-    	_broker.requestTransport(ORIGIN_1, DESTINATION_1, PRICE_1);
-    	TransportView transport1 = _broker.viewTransport(ID_1);
-		
-    	assertEquals(ID_1, transport1.getId());
-		assertEquals(ORIGIN_1, transport1.getOrigin());
-		assertEquals(DESTINATION_1, transport1.getDestination());
-		assertTrue(PRICE_1 == transport1.getPrice());
-		assertEquals(COMPANY_1_NAME, transport1.getTransporterCompany());
-		
-    	new Verifications() {{
-    		manager.getAllTransporterPorts(); times = 1;
-    		transporter.requestJob(anyString, anyString, anyInt); times = 1;
-    		manager.getTransporterPort(anyString); times = 1;
-    		transporter.jobStatus(anyString); times = 1;
-    		transporter2.requestJob(anyString, anyString, anyInt); times = 1;
-    	}};
-    }
 }
