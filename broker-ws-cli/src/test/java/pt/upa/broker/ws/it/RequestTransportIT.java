c86df534c500479f9ed48bc11e128c4db659f694
DiMesq <d.mesquita285@gmail.com>
2016-04-15 10:07:32 +0100

    Adds more IT tests for the broker client requestTransport method

--- broker-ws-cli/src/test/java/pt/upa/broker/ws/it/RequestTransportIT.java
+++ broker-ws-cli/src/test/java/pt/upa/broker/ws/it/RequestTransportIT.java
@@ -51,17 +51,40 @@ public class RequestTransportIT extends BaseBrokerIT{
     // No transporters operate in the given locations
     @Test
     public void testNoTransportAvailableForLocations() throws Exception{
-		String reply = client.requestTransport("Faro", "Porto", 30);
+		String reply = client.requestTransport("Faro", "Porto", 8);
 		assertEquals("Booking failed: No transporter is available", reply);
 		
     }
     
-    //TODO: test when the best price is greater than the reference price
+    //test when the best price is greater than the reference price
+    @Test
+    public void testNoTransportAvailableForGivenPrice() throws Exception{
+		String reply = client.requestTransport("Faro", "Lisboa", 30);
+		assertEquals("Booking failed: No transporter is available within price range", reply);
+		
+    }
     
     // First id of valid price must be 1
     @Test
     public void testReturnedTransportId() throws Exception{
-		String reply = client.requestTransport("Lisboa", "Lisboa", 30);
+		String reply = client.requestTransport("Lisboa", "Lisboa", 8);
 		assertEquals("1", reply);
     }
+    
+    // Test valid accentuated city names
+    @Test
+    public void testAccentuatedCityName() throws Exception{
+    	String accentuatedCity = "Setúbal";
+    	String reply = client.requestTransport("Lisboa", accentuatedCity, 8);
+		assertEquals("1", reply);
+    }
+    
+    // Test valid city names with spaces
+    @Test
+    public void testMultipleWordedCityName() throws Exception{
+    	String multipleWordedCity = "Castelo Branco";
+		String reply = client.requestTransport("Lisboa", multipleWordedCity, 8);
+		assertEquals("1", reply);
+    }
+    
 }
