1c62bff9fa90d1faeec16c602363c7ea9be8d118
DReigada <daniel.reigada@gmail.com>
2016-04-15 14:30:36 +0100

    Add the BaseTest class for unit tests in the broker

---
+++ broker-ws/src/test/java/pt/upa/broker/ws/BaseTest.java
@@ -0,0 +1,80 @@
+package pt.upa.broker.ws;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import mockit.Mocked;
+import pt.upa.transporter.ws.JobStateView;
+import pt.upa.transporter.ws.JobView;
+import pt.upa.transporter.ws.cli.TransporterClient;
+
+/**
+ *  Unit Test example
+ *  
+ *  Invoked by Maven in the "test" life-cycle phase
+ *  If necessary, should invoke "mock" remote servers 
+ */
+public class BaseTest {
+
+	// Static values to be used for comparing values
+	public static final String COMPANY_1_NAME = "UpaTransporter1";
+	public static final String COMPANY_2_NAME = "UpaTransporter2";
+	public static final String ORIGIN_1 = "Lisboa";
+	public static final String ORIGIN_2 = "Coimbra";
+	public static final String DESTINATION_1 = "Castelo Branco";
+	public static final String DESTINATION_2 = "Santarém";
+	public static final int PRICE_1 = 5;
+	public static final int PRICE_2 = 8;
+	public static final String ID_1 = "1";
+	public static final String ID_2 = "2";
+	public static final JobStateView STATE_1 = JobStateView.PROPOSED;
+	public static final JobStateView STATE_2 = JobStateView.PROPOSED;
+	
+	// the JobViews created to mock transporters responses
+	protected static JobView _jobView1, _jobView2;
+	
+	// initialize the jobViews
+	@BeforeClass
+	public static void oneTimeSetUp(){
+		_jobView1 = new JobView();
+		_jobView1.setCompanyName(COMPANY_1_NAME);
+		_jobView1.setJobOrigin(ORIGIN_1);
+		_jobView1.setJobDestination(DESTINATION_1);
+		_jobView1.setJobIdentifier(ID_1);
+		_jobView1.setJobPrice(PRICE_1);
+		_jobView1.setJobState(STATE_1);
+		
+		_jobView2 = new JobView();
+		_jobView2.setCompanyName(COMPANY_1_NAME);
+		_jobView2.setJobOrigin(ORIGIN_2);
+		_jobView2.setJobDestination(DESTINATION_2);
+		_jobView2.setJobIdentifier(ID_2);
+		_jobView2.setJobPrice(PRICE_2);
+		_jobView2.setJobState(STATE_2);
+	}
+	
+	@AfterClass
+	public static void oneTimeTearDown(){
+		_jobView1 = null;
+	}
+	
+	protected BrokerPort _broker;
+
+	@Mocked
+	protected TransporterClient transporter;
+	@Mocked 
+	protected TransporterCompaniesManager manager;
+	
+
+    @Before
+    public void setUp() {
+    	_broker = new BrokerPort();
+    }
+
+    @After
+    public void tearDown() {
+    	_broker = null;
+    }
+}
