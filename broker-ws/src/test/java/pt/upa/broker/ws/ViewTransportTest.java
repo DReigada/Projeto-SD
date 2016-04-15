1752159dcd6e2a5a7715ea1596f65a4e185b5e48
DiMesq <d.mesquita285@gmail.com>
2016-04-15 20:31:05 +0100

    Adds tests for the viewTransport with invalid id as input.

--- broker-ws/src/test/java/pt/upa/broker/ws/ViewTransportTest.java
+++ broker-ws/src/test/java/pt/upa/broker/ws/ViewTransportTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 
 import mockit.Expectations;
 import mockit.Verifications;
-import pt.upa.transporter.ws.BadLocationFault_Exception;
 
 public class ViewTransportTest extends BaseTest {
 	
@@ -18,6 +17,7 @@ public class ViewTransportTest extends BaseTest {
 	// TEST: invalid id non number
 	@Test
     public void testViewTransportWithInvalidIdOne() throws Exception{
+		
     	UnknownTransportFault fault = new UnknownTransportFault();
     	fault.setId(INVALID_ID_1);
     	
@@ -127,8 +127,8 @@ public class ViewTransportTest extends BaseTest {
     	}};
     }
 	
-	/* TESTS VALID ID */
-	// Only one element in _transports that is a valid transport
+	/* TESTS VALID ID 
+	// TEST: only one element in _transports (valid)
 	@Test
     public void testViewTransportWithOneElement() throws Exception{
     	UnknownTransportFault fault = new UnknownTransportFault();
@@ -136,26 +136,29 @@ public class ViewTransportTest extends BaseTest {
     	
     	new Expectations(){{
     		manager.getAllTransporterPorts(); result = Arrays.asList(transporter);
-    		transporter.requestJob(anyString, anyString, anyInt);
+    		transporter.requestJob(anyString, anyString, PRICE_1);
     		result = _jobView1;
     	}};
     	
     	_broker.requestTransport(ORIGIN_1, DESTINATION_1, PRICE_1);
+    	TransportView j = _broker.viewTransport("1");
+		
+    	assertEquals();
     	
-    	try {
-			_broker.viewTransport(null);
-			fail();
-		} catch (UnknownTransportFault_Exception e) {
-			assertEquals("No transports match the given transport identifier.", e.getMessage());
-			assertEquals(null, e.getFaultInfo().getId());
-		}
-    	
+    	_jobView1.setCompanyName(COMPANY_1_NAME);
+		_jobView1.setJobOrigin(ORIGIN_1);
+		_jobView1.setJobDestination(DESTINATION_1);
+		_jobView1.setJobIdentifier(ID_1);
+		_jobView1.setJobPrice(PRICE_1);
+		_jobView1.setJobState(STATE_1);
+		
     	new Verifications() {{
     		manager.getAllTransporterPorts(); times = 1;
     		transporter.requestJob(anyString, anyString, anyInt); times = 1;
     	}};
-    }
-	// Only one element in _transports that is an invalid transport
+    }*/
+	// TEST: only one element in _transports (invalid)
+	// TEST: only one element in _transports (valid) with transport price = reference price
 	// TEST: two valid transports, check one of them
 	// TEST: two invalid transports, check one of them
 	// TEST: one invalid one valid, test the invalid one
