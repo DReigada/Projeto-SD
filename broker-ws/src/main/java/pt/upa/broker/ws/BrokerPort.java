03df50e5b261ae77e24bfeee5ca1872343a06e8d
DReigada <daniel.reigada@gmail.com>
2016-05-06 19:26:48 +0100

    Move the BrokerTransportView to a file

--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -25,37 +25,6 @@ import pt.upa.transporter.ws.cli.TransporterClient;
 )
 public class BrokerPort implements BrokerPortType {
 
-  private class BrokerTransportView {
-    private String _transporterId;
-    public TransportView _transportView;
-
-    public BrokerTransportView(String id, String origin, String dest){
-      _transporterId = "";
-
-      TransportView t = new TransportView();
-      t.setId(id);
-      t.setOrigin(origin);
-      t.setDestination(dest);
-      t.setState(TransportStateView.REQUESTED);
-
-      _transportView = t;
-    }
-
-    // Transport view attribute setters
-    public void setTransportPrice(int price) { _transportView.setPrice(price); }
-    public void setTransportCompany(String company) { _transportView.setTransporterCompany(company); }
-    public void setTransportState(TransportStateView state) { _transportView.setState(state); }    
-
-    // Transport view attribute getters
-    public String getTransportCompany() { return _transportView.getTransporterCompany(); }
-    public TransportStateView getTransportState() { return _transportView.getState(); }
-    public TransportView getTransportView() { return _transportView; }
-
-    // Id given by transporter company setter and getter
-    public void setTransporterId(String id) { _transporterId = id; }
-    public String getTransporterId() { return _transporterId; }
-  }
-
   TransporterCompaniesManager _transportersManager;
   List<BrokerTransportView> _transports;
 
---
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerTransportView.java
@@ -0,0 +1,32 @@
+package pt.upa.broker.ws;
+
+public class BrokerTransportView {
+    private String _transporterId;
+    public TransportView _transportView;
+
+    public BrokerTransportView(String id, String origin, String dest){
+      _transporterId = "";
+
+      TransportView t = new TransportView();
+      t.setId(id);
+      t.setOrigin(origin);
+      t.setDestination(dest);
+      t.setState(TransportStateView.REQUESTED);
+
+      _transportView = t;
+    }
+
+    // Transport view attribute setters
+    public void setTransportPrice(int price) { _transportView.setPrice(price); }
+    public void setTransportCompany(String company) { _transportView.setTransporterCompany(company); }
+    public void setTransportState(TransportStateView state) { _transportView.setState(state); }    
+
+    // Transport view attribute getters
+    public String getTransportCompany() { return _transportView.getTransporterCompany(); }
+    public TransportStateView getTransportState() { return _transportView.getState(); }
+    public TransportView getTransportView() { return _transportView; }
+
+    // Id given by transporter company setter and getter
+    public void setTransporterId(String id) { _transporterId = id; }
+    public String getTransporterId() { return _transporterId; }
+  }
