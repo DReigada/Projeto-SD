b2e9f166458f3dd90ef8faec67e1d10f8efebdfc
DReigada <daniel.reigada@gmail.com>
2016-05-08 12:47:13 +0100

    Change the name of the interface to create the WSDL

---
+++ broker-ws/src/main/java/pt/upa/broker/backup/ws/BrokerBackupBase.java
@@ -0,0 +1,17 @@
+package pt.upa.broker.backup.ws;
+
+import javax.jws.Oneway;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import pt.upa.broker.ws.TransportView;
+
+@WebService
+public interface BrokerBackupBase {
+	
+	public void addTransport(@WebParam(name = "transport") TransportView transport, @WebParam(name = "transporterID") String transporterID);
+	
+	public void updateMessageCounter(@WebParam(name = "val") int val);
+	
+	@Oneway public void ping();
+}
