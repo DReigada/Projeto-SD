e33f465270915072c4b5063ce56455ff600d6afe
DReigada <daniel.reigada@gmail.com>
2016-05-13 16:26:05 +0100

    Fix: the order of the tearDown method was wrong

--- transporter-ws-cli/src/test/java/pt/upa/security/ws/RequestJobPriceAttackIT.java
+++ transporter-ws-cli/src/test/java/pt/upa/security/ws/RequestJobPriceAttackIT.java
@@ -1,34 +1,29 @@
 package pt.upa.security.ws;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNull;
 
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import pt.upa.transporter.ws.JobView;
 
 public class RequestJobPriceAttackIT extends BaseIT {
 	
-	@Before
-	public void setUp() throws Exception{
-    	transporter1.setIsTest(1);
-	}
-	
     // We assume that clearJobs it's working, or else some tests will fail
     @After
     public void tearDown(){
-    	super.tearDown();
     	transporter1.setIsTest(0);
+    	super.tearDown();
     }
 
     @Test
     public void testRequestJobModifyPrice() throws Exception{
     	String  city1 = "Lisboa",
     			city2 = "Coimbra";
-
+    	
+    	transporter1.setIsTest(1);
     	JobView job = transporter1.requestJob(city1, city2, 50);
     	assertNull(job);
     }
-    
+   
 } 
