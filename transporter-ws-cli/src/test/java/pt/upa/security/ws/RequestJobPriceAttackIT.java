36401a73888d3b614e2ca887c05932a1ab72da2b
DReigada <daniel.reigada@gmail.com>
2016-05-13 15:59:55 +0100

    Add setUp and tearDown methods to request job proce attack

--- transporter-ws-cli/src/test/java/pt/upa/security/ws/RequestJobPriceAttackIT.java
+++ transporter-ws-cli/src/test/java/pt/upa/security/ws/RequestJobPriceAttackIT.java
@@ -2,20 +2,33 @@ package pt.upa.security.ws;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import pt.upa.transporter.ws.JobView;
 
 public class RequestJobPriceAttackIT extends BaseIT {
-  @Test
-  public void testRequestJobModifyPrice() throws Exception{
-    String  city1 = "Lisboa",
-        city2 = "Coimbra";
+	
+	@Before
+	public void setUp() throws Exception{
+    	transporter1.setIsTest(1);
+	}
+	
+    // We assume that clearJobs it's working, or else some tests will fail
+    @After
+    public void tearDown(){
+    	super.tearDown();
+    	transporter1.setIsTest(0);
+    }
 
-    transporter1.setIsTest(1);
-    JobView job = transporter1.requestJob(city1, city2, 50);
-    assertNull(job);
-    System.out.println("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
-    transporter1.setIsTest(0);
-  }
+    @Test
+    public void testRequestJobModifyPrice() throws Exception{
+    	String  city1 = "Lisboa",
+    			city2 = "Coimbra";
+
+    	JobView job = transporter1.requestJob(city1, city2, 50);
+    	assertNull(job);
+    }
+    
 } 
